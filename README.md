ed2efeb6fcb5cf355f3a50fdf6bdb7d3f54d48b8
vladislav lis <lis.vladislav@gmail.com>
2023-06-23 01:53:40 +0300

    Adjusted readme; Formatted code in predict.py with flake8

--- README.md
+++ README.md
@@ -1,86 +1,37 @@
-# To run the script:
+# Car Price Prediction Model
 
-## 1. Copy repository / files.
-Clone the repository via github desktop or with "git clone https://github.com/vlad-lis/Ironhack-midterm.git" (terminal) or just download it as .zip and extract files.  
-You will only need "predict.py" and "data" folder; they need to be in the same folder.
+A linear regression model developed to predict the prices of used cars.
 
-## 2. Open terminal.
-In the terminal go to the directory which has the copied repository/files (from step #1):  
-for example (mac):  
-\$ cd Desktop/.. ../Ironhack-midterm  
-or windows:  
-\$ cd C:/Users/.. ../Ironhack-midterm
+To view all stages with comments, open `predict.ipynb` with Jupyter Notebook or an IDE that supports a Jupyter Python kernel.    
+No additional configuration should be required when using a conda environment and a conda Python kernel.  
+To run the script in the terminal or view the notebook file, please follow the instructions below.
 
-## 3. Launch the script from terminal.
-When in the directory with repository/files (step #2) type "python predict.py".  
-
-* The script may take a few seconds to run, don't panic :)
-* To exit/stop the script either type "no" when prompted with "Run the script?" or press ctrl+C at any time.
-* To clear the output in the terminal window after the script type "clear".
-  
-___________
-
-# Ironhack midterm project - Used cars price prediction
-
-[Project's repository](https://github.com/vlad-lis/Ironhack-midterm)
-
-## Members of the group:
-* Markus Schmidt
-* Vladislav Lis
-
-## Goal of the project:
-Creating a model to predict prices for used cards.
-
-## Description of the dataset:
-[Vehicle dataset](https://www.kaggle.com/datasets/nehalbirla/vehicle-dataset-from-cardekho) found on Kaggle.
-
-The combined dataset has up to 14,828 rows spread over 4 csvs. Every csv has a different amount of columns, different header names and the data is formatted in different styles.
-Columns range from 8 to 20 so we will most likely use 6 columns if there is not enough overlap.
-After the EDA and data cleaning we will most likely have to drop some rows and columns in case we cannot use all of the rows because of the aformentioned situation.
-
-## Planning:
-* Day 1 - EDA:  
-  * Getting insights from the data set;
-  * Checking for outliers and anomalies;
-  * Determining correlation between variables.
-* Day 2 - Cleaning the data:
-  * Removing null vales;
-  * Correcting data types;
-  * Checking for duplicates;
-  * Filtering unwanted outliers.
-* Day 3 - Transforming the data:
-  * Creating an overview pairplot (seaborn) to check for correlation;
-  * Scaling numerical features;
-  * Encoding categorical features.
-* Day 4 - Modelling:
-  * Creating a model;
-  * Testing the model;
-  * Comparing predicted and test results;
-  * Plotting test results;
-  * Presentation preparation.
-  * _optional:_
-    * tableau dashboard for the initial data set;
-* Day 5 - Presentation:
-  * Presenting the model.
+* `predict.py` prompts for user input to make a prediction.
+* `predict.ipynb` contains all stages of model development along with comments. The final function, which prompts for input and outputs the prediction, can be found at the end of the notebook.
 
 
+## Run locally (predict.py)
+1. Clone the repository.
+2. While in the root directory, install the dependencies via the terminal:  
+   - Activate the conda environment: `$ conda activate`  
+   - Alternatively, manually install pandas and scikit-learn into the current environment:
+     - macOS: `$ python3 -m pip install pandas` and `$ python3 -m pip install scikit-learn`
+     - Windows: `$ python -m pip install pandas` and `$ python -m pip install scikit-learn`
+3. While in the root directory, run `python3 predict.py` (macOS) or `$ python predict.py` (Windows)  
+   **Please note** that `predict.py` contains redundant code that creates the initial and input data frames.
+   This is intentionally left for demonstration purposes (code review) and will slightly slow down the initial execution of the script (usually by around 15 seconds, depending on your machine).
 
-## Day 1 - EDA:
-* EDA plan for data preparation before concatenation described in [.xls file ](https://github.com/vlad-lis/Ironhack-midterm/blob/main/EDA/EDA_planning.xlsx):
-![EDA plan](./EDA/EDAplan.png)
-* EDA results:
-  * 'car data.csv' has anomalies in feature values; the file is ignored in the analysis due to its low statistical value with only 300 rows.
-  * 'model' and 'engine' categorical columns have too many unique values; these columns will be ignored for the final data set due to the amount of dummified columns they would create after normalizing the data.
-  * 'seats' column has nine unique values and will be used as the closest present alternative to vehicle type.
-  * in order to concatenate the csv files into one dataframe columns 'location', 'color', 'mileage', 'max power', 'max torque', 'drivetrain', 'length', 'width', 'height', 'fuel tank capacity' will be ignored, since they are not present in each file.
-  * concatenating three csv files will result in a dataframe with roughly over 14k observations.
-  * no outliers require removal.
+## Run/view locally (predict.ipynb) 
+1. Clone the repository.
+2. To preview:
+   - With Jupyter Notebook: Simply open the file in Jupyter Notebook; nothing extra is required.  
+   - With VSCode: 
+     - While in the root directory, install the dependencies: `$ pip install -r requirements.txt` (contains extras along with core ones).
+     - Switch to the conda Python kernel.
 
-## Day 2 - Data cleaning results:
-* extracted the first word (Make) in the 'name' and 'owner' columns.
-* dropped extra columns.
-* formatted 'selling price' column.
-* concatenated three files into a new dataframe.
-* checked for null values (there aren't any).
-* checked for duplicates (2.2k duplicated found) and droppped them.
-* check data types (all correct).
+## Dataset
+The [Vehicle dataset](https://www.kaggle.com/datasets/nehalbirla/vehicle-dataset-from-cardekho) can be found on Kaggle.
+
+The combined dataset consists of up to 14,828 rows spread across 4 CSV files (approximately 10,485 after data cleaning).
+Each CSV file has a different number of columns, different header names, and different data formatting.
+The number of columns ranges from 8 to 20.
--- predict.py
+++ predict.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import statistics as stats
 from sklearn.preprocessing import PowerTransformer, StandardScaler
-
 from scipy.stats import iqr
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -17,15 +15,15 @@ df2['name'] = df2['name'].apply(lambda x: x.split()[0])
 df1['owner'] = df1['owner'].apply(lambda x: x.split()[0])
 df2['owner'] = df2['owner'].apply(lambda x: x.split()[0])
 
-df3.rename(columns=
-           {"Year": "year",
-            "Price": "selling_price",
-            "Kilometer": "km_driven", 
-            "Fuel Type": "fuel", 
-            "Seller Type": "seller_type", 
-            "Transmission": "transmission", 
-            "Make": "name",
-            "Owner": "owner"},inplace=True)
+df3.rename(columns={
+    "Year": "year",
+    "Price": "selling_price",
+    "Kilometer": "km_driven",
+    "Fuel Type": "fuel",
+    "Seller Type": "seller_type",
+    "Transmission": "transmission",
+    "Make": "name",
+    "Owner": "owner"}, inplace=True)
 
 df1["selling_price"] /= 100
 df1["selling_price"] = df1["selling_price"].astype(int)
@@ -35,23 +33,26 @@ df3["selling_price"] /= 100
 df3["selling_price"] = df3["selling_price"].astype(int)
 
 
-df2.drop(['mileage', 'engine', 'max_power', 'torque', 'seats'], axis=1, inplace=True)
-df3.drop(['Model', 'Location', 'Color', 'Engine', 'Max Power', 'Max Torque', 
-          'Drivetrain', 'Length', 'Width', 'Height', 'Seating Capacity', 'Fuel Tank Capacity'], axis=1, inplace=True)
+df2.drop(['mileage', 'engine', 'max_power',
+         'torque', 'seats'], axis=1, inplace=True)
+df3.drop(['Model', 'Location', 'Color', 'Engine', 'Max Power', 'Max Torque',
+          'Drivetrain', 'Length', 'Width', 'Height', 'Seating Capacity',
+          'Fuel Tank Capacity'], axis=1, inplace=True)
 
 df = pd.concat([df1, df2, df3], ignore_index=True)
 
-#check for duplicates
+# check for duplicates
 dup = df[df.duplicated(keep=False)].sort_values(by=['selling_price'])
 
-#drop duplicates
+# drop duplicates
 df.drop_duplicates(inplace=True, ignore_index=True)
 
-#change 'year' to 'age' (and count it)
+# change 'year' to 'age' (and count it)
 df.rename(columns={'year': 'age'}, inplace=True)
 df['age'] = df['age'].apply(lambda x: df['age'].max()+1-x)
 
-#clean values in 'fuel' column
+
+# clean values in 'fuel' column
 def clean_fuel(value):
     if 'CNG' in value:
         value = 'CNG'
@@ -59,9 +60,11 @@ def clean_fuel(value):
         value = 'Petrol'
     return value
 
+
 df['fuel'] = df['fuel'].apply(lambda x: clean_fuel(x))
 
-#transform values in 'owner' column
+
+# transform values in 'owner' column
 def clean_owner(value):
     if value == 'Test' or value == 'UnRegistered Car':
         value = 0
@@ -77,10 +80,11 @@ def clean_owner(value):
         value = 5
     return value
 
+
 df['owner'] = df['owner'].apply(lambda x: clean_owner(x))
 
 
-#clean 'seller_type' column
+# clean 'seller_type' column
 def clean_seller(value):
     if 'Dealer' in value:
         value = 'Dealer'
@@ -88,18 +92,19 @@ def clean_seller(value):
         value = 'Corporate'
     return value
 
+
 df['seller_type'] = df['seller_type'].apply(lambda x: clean_seller(x))
 
 
-#remove outliers
+# remove outliers
 def remove_outliers(df):
     for c in df.columns:
-            pct_75 = np.percentile(df[c], 75)
-            pct_25 = np.percentile(df[c], 25)
-            upper_bound = pct_75 + 1.5*iqr(df[c])
-            lower_bound = pct_25 - 1.5*iqr(df[c])
-            condition = (df[c] < upper_bound) & (df[c] > lower_bound)
-            df[c] = df[c][condition]
+        pct_75 = np.percentile(df[c], 75)
+        pct_25 = np.percentile(df[c], 25)
+        upper_bound = pct_75 + 1.5*iqr(df[c])
+        lower_bound = pct_25 - 1.5*iqr(df[c])
+        condition = (df[c] < upper_bound) & (df[c] > lower_bound)
+        df[c] = df[c][condition]
     return df
 
 
@@ -112,7 +117,7 @@ df = pd.concat([numerical, categorical], axis=1)
 df.dropna(inplace=True)
 
 
-#X, y & train, test split
+# X, y & train, test split
 X = df.drop('selling_price', axis=1)
 y = df[['selling_price']].copy()
 
@@ -122,7 +127,7 @@ catX = X.select_dtypes(include=[object])
 
 numericalX_columns = numericalX.columns
 
-#dummies
+# dummies
 numericalX = pd.DataFrame(numericalX, columns=numericalX_columns)
 categoricalX = pd.get_dummies(categoricalX, drop_first=True)
 
@@ -132,19 +137,18 @@ categoricalX.reset_index(drop=True, inplace=True)
 X = pd.concat([categoricalX, numericalX], axis=1, copy=False)
 
 
-#train, test split
+# train, test split
 tt_ratio = 0.3
 rand_seed = 40
 
 
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=tt_ratio, random_state=rand_seed)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=tt_ratio, random_state=rand_seed)
 X_train = pd.DataFrame(X_train)
 X_test = pd.DataFrame(X_test)
 
 
-
-#scaler & transformer (train data)
+# scaler & transformer (train data)
 X_train_num = X_train[numericalX_columns]
 X_train_cat = X_train.drop(numericalX_columns, axis=1)
 
@@ -161,7 +165,7 @@ X_train_num.reset_index(drop=True, inplace=True)
 X_train_cat.reset_index(drop=True, inplace=True)
 X_train = pd.concat([X_train_cat, X_train_num], axis=1, copy=False)
 
-#scaler & transformer (test data)
+# scaler & transformer (test data)
 X_test_num = X_test[numericalX_columns]
 X_test_cat = X_test.drop(numericalX_columns, axis=1)
 
@@ -174,22 +178,24 @@ X_test_cat.reset_index(drop=True, inplace=True)
 X_test = pd.concat([X_test_cat, X_test_num], axis=1, copy=False)
 
 
-### Input -> prediction
+# Input -> prediction
 
 # function for numerical column input
 def ask_input_num(question, options):
     while True:
-        prompt = input(f'enter {question}: ') 
+        prompt = input(f'enter {question}: ')
         try:
             if int(prompt) not in options:
-                print(f' ! pick something between {min(options)} and {max(options)}')
+                print(
+                    f' ! pick something between {min(options)}'
+                    f' and {max(options)}')
             else:
                 return prompt
         except ValueError:
-            print(' ! enter a number with no decimals')     
-        
+            print(' ! enter a number with no decimals')
 
-#function for categorical column input
+
+# function for categorical column input
 def ask_input_cat(question, options):
     while True:
         prompt = input(f'enter {question}: ')
@@ -199,7 +205,7 @@ def ask_input_cat(question, options):
             return prompt
 
 
-#prediction with input
+# prediction with input
 def prediction():
     print("""
     ******************************
@@ -209,7 +215,7 @@ def prediction():
     t = X_test.iloc[:1].copy()
     t = pd.DataFrame(t)
     t.iloc[:] = 0
-    
+
     list_of_models = list(df['name'].unique())
     list_of_age = list(range(51))
     list_of_km = list(range(300001))
@@ -217,7 +223,7 @@ def prediction():
     list_of_fuel = list(df['fuel'].unique())
     list_of_sellers = list(df['seller_type'].unique())
     list_of_transmission = list(df['transmission'].unique())
-    
+
     model = ask_input_cat('model name', list_of_models)
     age = ask_input_num('car\'s age', list_of_age)
     km = ask_input_num('car\'s distance travelled', list_of_km)
@@ -225,7 +231,7 @@ def prediction():
     fuel = ask_input_cat('fuel type', list_of_fuel)
     seller = ask_input_cat('seller type', list_of_sellers)
     transmission = ask_input_cat('transmission type', list_of_transmission)
-    
+
     t.at[0, f'name_{model}'] = 1
     t.at[0, 'age'] = age
     t.at[0, 'km_driven'] = km
@@ -233,7 +239,7 @@ def prediction():
     t.at[0, f'fuel_{fuel}'] = 1
     t.at[0, f'seller_type_{seller}'] = 1
     t.at[0, f'transmission_{transmission}'] = 1
-    
+
     if model == 'Ambassador':
         t.drop(['name_Ambassador'], axis=1, inplace=True)
     if fuel == 'CNG':
@@ -242,7 +248,7 @@ def prediction():
         t.drop(['seller_type_Corporate'], axis=1, inplace=True)
     if transmission == 'Automatic':
         t.drop(['transmission_Automatic'], axis=1, inplace=True)
-    
+
     t_num = t[numericalX_columns]
     t_cat = t.drop(numericalX_columns, axis=1)
 
@@ -250,32 +256,32 @@ def prediction():
     t_num = pt.transform(t_num)
     t_num = pd.DataFrame(t_num, columns=numericalX_columns)
 
-    t = pd.concat([t_cat, t_num], axis=1, copy=False) 
-    
+    t = pd.concat([t_cat, t_num], axis=1, copy=False)
+
     model = LinearRegression()
     model.fit(X_train, y_train)
     price = model.predict(t).tolist()[0][0]
-    
+
     if price < 0:
         price = 0
     else:
         price = round(price)
-    
+
     if price == 0:
-        output = 'Sorry, but the model says this car is a worthless piece of junk :('
+        output = 'Sorry, but the model says this car is a worthless :('
     else:
         output = f'Predicted price is: {price} $'
-    
+
     out = print(f"""
     *******************************
     {output}
     *******************************
     """)
-        
+
     return out
 
 
-#wrapper function for multiple runs
+# wrapper function for multiple runs
 def wrapper(func):
     while True:
         ask = input('Run the script? (yes/no) ')
@@ -283,15 +289,11 @@ def wrapper(func):
             func()
         elif ask == 'no':
             print("""
-        Done:)
+        Done :)
             """)
             break
         else:
             print(' ! type "yes" or "no"')
 
+
 wrapper(prediction)
-
-
-
-
-
